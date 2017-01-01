28beb71052d637b3adc6846d9a366667c5252217
weiruiwei <weiruiwei@163.com>
2017-01-01 15:03:24 +0800

    xx

--- src/main/java/com/bauer/utils/StringUtils.java
+++ src/main/java/com/bauer/utils/StringUtils.java
@@ -2,6 +2,14 @@ package com.bauer.utils;
 
 public class StringUtils {
 
+	/**
+	 * @Title isEmpty
+	 * @Description 
+	 * @param str
+	 * @return
+	 * @author bauer
+	 * @date 2017年1月1日 下午3:00:55
+	 */
 	public static boolean isEmpty(String str){
 		if(str==null){
 			return true;
